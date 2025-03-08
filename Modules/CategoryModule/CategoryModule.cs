5b1b69115910f7171bbd16b59d65bc1a0e8cb0d6
Muhammet Kılınc <108901980+muhammetkilinc15@users.noreply.github.com>
2025-03-08 17:53:50 +0300

    Add category endpoint

---
+++ Modules/CategoryModule/CategoryModule.cs
@@ -0,0 +1,24 @@
+﻿using Carter;
+using GithubActionTest.Models;
+
+namespace GithubActionTest.Modules.CategoryModule
+{
+    public class CategoryModule : ICarterModule
+    {
+        public void AddRoutes(IEndpointRouteBuilder app)
+        {
+            IEndpointRouteBuilder group = app.MapGroup("/api/categories");
+
+            group.MapGet("", () =>
+            {
+                List<Category> categories =
+                [
+                    new() { Id = 1, Name = "Category 1" },
+                    new() { Id = 2, Name = "Category 2" },
+                    new() { Id = 3, Name = "Category 3" }
+                ];
+                return Results.Ok(categories);
+            }).Produces<List<Category>>();
+        }
+    }
+}
